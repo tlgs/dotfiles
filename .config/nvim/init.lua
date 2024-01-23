a01dc84bbd47d64ed349de40b249190ddd24a57f
Tiago Seabra <tlgs@users.noreply.github.com>
2024-01-23 15:20:41 +0000

    Reset Neovim configuration
    Currently using some subset of kickstart.nvim's set options.

---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+-- kickstart.nvim set options:
+vim.o.hlsearch = false                  -- set highlight on search
+vim.wo.number = true                    -- make line numbers default
+vim.o.mouse = 'a'                       -- enable mouse mode
+vim.o.clipboard = 'unnamedplus'         -- sync clipboard between OS and Neovim
+vim.o.breakindent = true                -- wrapped lines continue visually indented
+vim.o.undofile = true                   -- save undo history
+vim.o.ignorecase = true                 -- case-insensitive searching
+vim.o.smartcase = true
+vim.o.updatetime = 250                  -- decrease update time
+vim.o.timeoutlen = 300
+vim.o.completeopt = 'menuone,noselect'  -- better completion experience
+vim.o.termguicolors = true              -- enable 24-bit RG
